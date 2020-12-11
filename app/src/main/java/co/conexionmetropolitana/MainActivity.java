b619c8013b7a3bfb7709b8977b07fab5c6de46c9
cervialsas <75081662+cervialsas@users.noreply.github.com>
2020-12-11 10:00:50 -0500

    5 - Segunda publicación para pruebas internas

--- app/src/main/java/co/conexionmetropolitana/MainActivity.java
+++ app/src/main/java/co/conexionmetropolitana/MainActivity.java
@@ -1,7 +1,13 @@
 package co.conexionmetropolitana;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -30,13 +36,17 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                /* Los dominios principales de esta app */
                 if(Uri.parse(url).getHost().endsWith("mitiendametropolitana.com")||Uri.parse(url).getHost().endsWith("cetiia.com.co")){
                     imageView.setVisibility(View.VISIBLE);
                     Snackbar.make(webView, "Cargando...", Snackbar.LENGTH_LONG).setAction("Action", null).show();
                     return false;
                 }
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                view.getContext().startActivity(intent);
+
+                /* Control de salida hacia redes sociales */
+                if(Uri.parse(url).getHost().endsWith("facebook.com")||Uri.parse(url).getHost().endsWith("instagram.com")||Uri.parse(url).getHost().endsWith("twitter.com")||Uri.parse(url).getHost().endsWith("wa.me")){
+                    confirmarSalir(view,url);
+                }
                 return true;
             }
             @Override
@@ -67,4 +77,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     /* Start custom methods */
+
+    /*  */
+    public void confirmarSalir(View view, String url){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Saliendo de conexion metropolitana");
+        builder.setMessage("Desea salir de la app?");
+        builder.setPositiveButton("Salir", (dialog, which) -> {
+            /* Se sale de la app */
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            view.getContext().startActivity(intent);
+        });
+        builder.setNegativeButton("Quedarme", (dialog, which) -> {
+            /*  */
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }
